7895792920469670387612ddc60b0005dcffc8ad
hxzwww <vakhlov.av@phystech.edu>
2022-04-23 19:34:49 +0300

    first try

---
+++ task5/cmake-with-flask/B/CMakeFiles/ClassLib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../lib/libClassLib.pdb"
+  "../lib/libClassLib.so"
+  "CMakeFiles/ClassLib.dir/lib.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ClassLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
